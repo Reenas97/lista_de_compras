f39e682cd4eb8caf6204484d0b51e570d33b5383
Renata Sanchez <91281826+Reenas97@users.noreply.github.com>
2025-11-29 18:09:04 +0000

    criacao da tela de detalhes do produto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,14 @@
 class ProductsController < ApplicationController
   def show
+    @product = Product.find(params[:id])
   end
 
   def index
     @products = Product.all
+  end
+
+  private
+
+  def products_parans
   end
 end
