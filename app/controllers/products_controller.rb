3846b8b380b2bfb565b3e14d12148a8af769eab3
Renata Sanchez <91281826+Reenas97@users.noreply.github.com>
2025-11-29 20:00:31 +0000

    Criação de telas de Produto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,8 +7,43 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create 
+    @product = Product.new(products_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
+  end
+  
+  def edit 
+    @product = Product.find(params[:id])
+  end
+
+  def update
+     @product = Product.find(params[:id])
+    if @product.update(products_params)
+      redirect_to @product, notice: 'Ebaaa salvou'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+     @product = Product.find(params[:id])
+     @product.destroy
+     redirect_to products_path
+  end
+
+
+
   private
 
-  def products_parans
+  def products_params
+    params.require(:product).permit(:name, :price, :quantity)
   end
 end
